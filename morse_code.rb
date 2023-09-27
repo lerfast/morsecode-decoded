86a60f280d42bb95afa7ea6d2213f31d192dd50b
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-09-27 11:58:03 -0500

    Create morse_code.rb
    Co-Authored-By: Juan Diego Guerra <jdxelric@gmail.com>

---
+++ morse_code.rb
@@ -0,0 +1,23 @@
+MORSE_CODE = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(morse_char)
+  MORSE_CODE[morse_char] || ' '
+end
+
+def decode_word(morse_word)
+  morse_word.split.map { |morse_char| decode_char(morse_char) }.join
+end
+
+def decode_message(morse_message)
+  morse_words = morse_message.split('   ')
+  morse_words.map { |morse_word| decode_word(morse_word) }.join(' ')
+end
+
+
