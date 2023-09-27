fb68ab8c01fcbce065f9cc5af0126a7e36422a9f
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-09-27 14:43:09 -0500

    Update morsecode.rb
    Running Linter, running Rubocop

--- morsecode.rb
+++ morsecode.rb
@@ -1,26 +1,25 @@
 MORSE_CODE = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-    '--..' => 'Z'
-  }.freeze
-  
-  def decode_char(morse_char)
-    MORSE_CODE[morse_char] || ' '
-  end
-  
-  def decode_word(morse_word)
-    morse_word.split.map { |morse_char| decode_char(morse_char) }.join
-  end
-  
-  def decode_message(morse_message)
-    morse_words = morse_message.split('   ')
-    morse_words.map { |morse_word| decode_word(morse_word) }.join(' ')
-  end
-  
-  morse_code_message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
-  decoded_message = decode_message(morse_code_message)
-  puts decoded_message
-  
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(morse_char)
+  MORSE_CODE[morse_char] || ' '
+end
+
+def decode_word(morse_word)
+  morse_word.split.map { |morse_char| decode_char(morse_char) }.join
+end
+
+def decode_message(morse_message)
+  morse_words = morse_message.split('   ')
+  morse_words.map { |morse_word| decode_word(morse_word) }.join(' ')
+end
+
+morse_code_message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_message = decode_message(morse_code_message)
+puts decoded_message
